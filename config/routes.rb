c1e6a0f3725b62afb3831b5eed5a4f8178d2b986
Vadim Dimnich <v.dimnich@gmail.com>
2019-09-21 18:14:21 +0300

    hw: create route home/contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  get '/' => 'home#index'
 
+  # Перенаправил маршрут на главную страницу
+  get '/' => 'home#index'
+  
+  # Создаю маршрут, простой
+  get 'home/contacts'
+
+  # Создаю маршрут, по REST архитектуре
   resources :articles
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
