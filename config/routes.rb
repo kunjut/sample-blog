01a735322573d4d5f9f0d00fa7debff0a78697eb
Vadim Dimnich <v.dimnich@gmail.com>
2019-09-21 22:03:28 +0300

    update routes after conflict in merge

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,9 @@ Rails.application.routes.draw do
 
   # Перенаправил маршрут на главную страницу
   get '/' => 'home#index'
-<<<<<<< HEAD
   
-  # Создаю маршрут, простой
-  get 'home/contacts'
-=======
   get '/contacts' => 'contacts#new'
   post '/contacts' => 'contacts#create'
->>>>>>> x001
 
   # Создаю маршрут, по REST архитектуре
   resources :articles
